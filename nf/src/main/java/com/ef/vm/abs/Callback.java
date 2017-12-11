566ba18f050011214fc4a433cb64db327f82c494
andyoom <935439276@qq.com>
2017-12-11 16:50:19 +0800

    Initial commit

---
+++ nf/src/main/java/com/ef/vm/abs/Callback.java
@@ -0,0 +1,9 @@
+package com.ef.vm.abs;
+
+/**
+ * @author Lody
+ */
+
+public interface Callback<T> {
+    void callback(T result);
+}
---
+++ nf/src/main/java/com/ef/vm/vs/VSManagerActivity.java
@@ -0,0 +1,13 @@
+package com.ef.vm.vs;
+
+
+import com.ef.vm.abs.ui.VActivity;
+
+/**
+ * @author Lody
+ *
+ *
+ *
+ */
+public class VSManagerActivity extends VActivity {
+}
