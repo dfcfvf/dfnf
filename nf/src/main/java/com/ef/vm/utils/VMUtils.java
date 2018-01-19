705af9c8932d45a7c63af73f5436b7d9cc0f5b60
dfcfvf <33210689+dfcfvf@users.noreply.github.com>
2018-01-19 22:03:59 +0800

    111

--- nf/src/main/java/com/ef/vm/utils/VMUtils.java
+++ nf/src/main/java/com/ef/vm/utils/VMUtils.java
@@ -20,12 +20,74 @@ import java.util.List;
  */
 
 public class VMUtils {
+
+    /**
+     * 判断是否安装
+     *
+     * @param packageName
+     * @return
+     */
+    public static boolean isAppInstalledAsUser(String packageName) {
+        try {
+            return VirtualCore.get().isAppInstalledAsUser(0, packageName);
+        } catch (Exception e) {
+            uninstallApp(packageName);
+            return false;
+        }
+    }
+
+    /**
+     * 判断是否运行
+     *
+     * @param packageName
+     * @return
+     */
+    public static boolean isAppRunning(String packageName) {
+        return isRunning(packageName);
+    }
+
+    /**
+     * 卸载App
+     *
+     * @param packageName
+     */
+    public static boolean uninstallApp(String packageName) {
+        VirtualCore core = VirtualCore.get();
+        try {
+            return core.uninstallPackage(packageName);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * 安装app
+     */
+    public static boolean installapp(String pkg) {
+        VirtualCore core = VirtualCore.get();
+        if (isAppInstalledAsUser(pkg)) {
+            return true;
+        }
+        ApplicationInfo info = null;
+        try {
+            info = VirtualCore.get().getUnHookPackageManager().getApplicationInfo(pkg, 0);
+            if (info == null || info.sourceDir == null) {
+                return false;
+            }
+            InstallResult result = core.installPackage(info.sourceDir, InstallStrategy.DEPEND_SYSTEM_IF_EXIST);
+            return result.isSuccess;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
     /**
      * 安装app
      */
     public static void installapp_old(Context context, String pkg, int userId) {
         AppInfoLite infolite = getAppInfoLiteByPkg(context, pkg);
-        if(infolite!=null){
+        if (infolite != null) {
             addVirtualApp(infolite);
         }
     }
@@ -60,7 +122,7 @@ public class VMUtils {
     public static void installapps_old(Context context, List<String> list, int userId) {
         for (String packageName : list) {
             AppInfoLite infolite = getAppInfoLiteByPkg(context, packageName);
-            if(infolite!=null){
+            if (infolite != null) {
                 addVirtualApp(infolite);
             }
         }
@@ -91,28 +153,17 @@ public class VMUtils {
             }
         }
     }
-    public static void uninstallApp(String pkg){
-        VirtualCore core = VirtualCore.get();
-        try{
-            core.uninstallPackage(pkg);
-            if(!"com.ztgame.bob".equals(pkg)){
-                core.uninstallPackage("com.tencent.mobileqq");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 
-    public static boolean isRunning(String pkg){
+    public static boolean isRunning(String pkg) {
         VirtualCore core = VirtualCore.get();
         if (core.isAppInstalledAsUser(0, pkg)) {
             return core.isAppRunning(pkg, 0);
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static AppInfoLite getAppInfoLiteByPkg(Context context, String packagename){
+    public static AppInfoLite getAppInfoLiteByPkg(Context context, String packagename) {
         AppInfoLite infolite = null;
         try {
             PackageInfo pkg = context.getPackageManager().getPackageInfo(packagename, 0);
@@ -137,7 +188,7 @@ public class VMUtils {
     }
 
     public static InstallResult addVirtualApp(AppInfoLite info) {
-        if(VirtualCore.get().isAppInstalled(info.packageName)) {
+        if (VirtualCore.get().isAppInstalled(info.packageName)) {
             return null;
         }
         int flags = InstallStrategy.COMPARE_VERSION | InstallStrategy.SKIP_DEX_OPT;
@@ -152,10 +203,10 @@ public class VMUtils {
      * yekh 2017-10-2
      */
     public static void uninstallQQ() {
-        Log.e("home","uninstall QQ");
-        try{
+        Log.e("home", "uninstall QQ");
+        try {
             VirtualCore.get().uninstallPackage("com.tencent.mobileqq");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
